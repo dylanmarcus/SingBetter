a9b7f81cf1015de882188f80a422757d6a664be2
Ruler-of-Chaos <timelord255@hotmail.com>
2017-04-01 12:08:17 -0700

    Implemented pitch detection in the app using TarsosDSP library

--- SingBetter/app/src/main/java/com/inyourface/singbetter/MainActivity.java
+++ SingBetter/app/src/main/java/com/inyourface/singbetter/MainActivity.java
@@ -2,12 +2,48 @@ package com.inyourface.singbetter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
+import be.tarsos.dsp.pitch.PitchProcessor.PitchEstimationAlgorithm;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView freqText;
+    private Button freqButton;
+    float pitchInHz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pitchInHz = 0;
+        freqText = (TextView) findViewById(R.id.freq_text);
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult result,AudioEvent e) {
+                pitchInHz = result.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        freqText.setText("" + pitchInHz);
+                    }
+                });
+            }
+        };
+        AudioProcessor p = new PitchProcessor(PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(p);
+        Thread t = new Thread(dispatcher,"Audio Dispatcher");
+        t.start();
+        freqText.setText("" + pitchInHz + " Hz");
     }
 }
